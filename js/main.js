38a2cbfce9971899e068eb69358dfabbab53a048
Anthony <trecool9@gmail.com>
2024-12-15 17:49:08 +0200

    25_26.0.1

--- js/main.js
+++ js/main.js
@@ -1,92 +1,115 @@
-console.log('#6. JavaScript homework example file')
+console.log('#8. JavaScript homework example file')
+
 /*
  * #1
- * Задача: Калькулятор калорійності продуктів через клас.
- * Завдання: Розробити модуль на JavaScript, який імплементує клас CalorieCalculator. Клас має використовувати Map для управління даними про калорійність продуктів. 
- * Необхідно реалізувати наступні функціональності:
- * Додавання продуктів: Метод addProduct приймає назву продукту та його калорійність, додаючи їх до колекції.
- * Отримання калорійності продукту: Метод getProductCalories повертає калорійність продукту за його назвою. Якщо продукт не знайдено, повертає рядок 'Product not found'.
- * Видалення продукту: Метод removeProduct видаляє продукт з колекції за назвою.
  *
- * Критерії перевірки:
- * Клас CalorieCalculator має бути реалізований з використанням ключового слова class.
- * Внутрішнє сховище продуктів має бути реалізоване за допомогою new Map().
- * Наявність методів addProduct, getProductCalories, та removeProduct.
+ * Задача: Створення та додавання DOM-елемента до вказаного контейнера
+ * Мета: Розробити функцію createDomElement, яка приймає назву тега, текстовий вміст та контейнер, до якого потрібно додати новий елемент. Функція створює новий елемент з вказаним тегом та текстовим вмістом і додає цей елемент до заданого контейнера.
+ *
+ * Вимоги:
+ * 1. Функція має приймати три параметри:
+ *    - tagName - рядок, що вказує на назву тега нового елемента.
+ *    - textContent - рядок, що вказує на текстовий вміст нового елемента.
+ *    - container - DOM-елемент, до якого буде додано новий створений елемент.
+ * 2. Функція має створити новий DOM-елемент з вказаним тегом і текстовим вмістом.
+ * 3. Створений елемент має бути доданий до вказаного контейнера.
+ * 4. Функція повертає посилання на створений елемент, що дозволяє подальшу взаємодію з ним.
+ * 5. Функція має бути експортована для використання в інших модулях та тестування.
  */
-class CalorieCalculator {
-  constructor() {
-    this.ProductCalorie = new Map();;
-  }
 
-  addProduct(productName, calories) {
-    this.ProductCalorie.set(productName, calories);
-  }
-  getProductCalories(productName) {
-    // Отримуємо калорійність з Map
-    const calories = this.ProductCalorie.get(productName);
-     // Перевірка наявності продукту
-    return calories !== undefined 
-    // Якщо продукт знайдено - повертаємо його калорійність
-    ? calories 
-    // Якщо не знайдено - повертаємо текстове повідомлення
-    : 'Product not found'
-  }
+function createDomElement(tagName, textContent, container) {
 
-  removeProduct(productName) {
-    this.ProductCalorie.delete(productName)
-  }
+  // створення елементу "в даному випадку створюємо параграф"
+  const el = document.createElement(tagName); 
+
+  //додаю через властивість textContent 
+  el.textContent = textContent; 
+
+  // додавання елементу в контейнер  
+  container.appendChild(el); 
+  return el;
 }
-// Демонстрація використання
-const calorieCalculator = new CalorieCalculator()
-calorieCalculator.addProduct('Apple', 52)
-calorieCalculator.addProduct('Banana', 89)
-console.log(calorieCalculator.getProductCalories('Apple')) // 52
-document.write(`<br> Калорійність Яблука: ${calorieCalculator.getProductCalories('Apple')} ккал<br>`)
-console.log(calorieCalculator.getProductCalories('Banana')) // 89
-document.write(`<br> Калорійність Банана: ${calorieCalculator.getProductCalories('Banana')} ккал<br>`)
-calorieCalculator.removeProduct('Apple')
-console.log(calorieCalculator.getProductCalories('Apple')) // Product not found
-document.write(`<br> Калорійність Яблука: ${calorieCalculator.getProductCalories('Apple')} ккал<br>`)
+
+// Демонстрація використання функції
+
+// В якості прикладу використовуємо body як контейнер
+const container = document.body 
+console.log(createDomElement('p', 'This paragraph has been added to the specified container.', container))
+
 /*
-
  * #2
- * Задача: Унікальні користувачі.
- * Завдання: Реалізувати модуль на JavaScript у формі класу UniqueUsernames, який використовує Set для збереження унікальних імен користувачів. Клас має надавати можливість:
- * Додавання імен користувачів: Метод addUser дозволяє додати нове ім'я до набору. Якщо ім'я вже існує, воно не буде додано повторно, зберігаючи унікальність імен у наборі.
- * Перевірка наявності імені: Метод exists перевіряє, чи існує задане ім'я серед збережених унікальних імен.
- * Отримання кількості унікальних імен: Метод count повертає кількість унікальних імен, збережених у наборі.
  *
- * Критерії перевірки:
- * Наявність методів addUser, exists, count у класі UniqueUsernames.
- * Використання конструкції class для створення класу UniqueUsernames.
- * Застосування new Set() для внутрішнього сховища імен користувачів у конструкторі класу.
+ * Задача: Встановлення cookie з корисною інформацією на 10 секунд
+ * Мета: Розробити функцію setUserInfoCookie, яка встановлює cookie з ім'ям userInfo та значенням у форматі "ключ=значення", яке зберігає корисну інформацію про користувача (наприклад, обрану мову інтерфейсу) та має термін дії 10 секунд. Значення cookie повинно бути відповідно закодовано для безпечного зберігання у веб-браузері.
+ *
+ * Вимоги до функції:
+ *
+ * 1. Функція приймає два аргументи: key (назва інформаційного параметра) та value (значення параметра).
+ * 2. Функція кодує значення параметра для коректного зберігання у cookie.
+ * 3. Функція встановлює cookie userInfo з закодованим значенням "ключ=значення" та встановлює термін його дії на 10 секунд.
+ * 4. При встановленні cookie, функція виводить інформаційне повідомлення у консоль про успішне зберігання даних.
  */
-class UniqueUsernames {
-  constructor() {
-    this.userNames = new Set();
-  }
 
-  addUser(name) {
-    this.userNames.add(name.toLowerCase()) //Конвертує ім'я в нижній регістр для уникнення реєстрозалежності
-  }
+// setUserInfoCookie.js
 
-  exists(name) {
-    return this.userNames.has(name.toLowerCase())//Перевіряє наявність імені в Set
-  }
+function setUserInfoCookie(key, value) {
 
-  count() {
-    return this.userNames.size;
-  }
+  // Функція кодує значення gпараметра в кукі, кодуємо(НЕ ШИФРУВАННЯ) ключ-значення в якому воно повино виглядати.                    
+  const encoded = encodeURIComponent(`${key} = ${value}`); 
+  const date = new Date();
+
+  //Налаштовую для cookie термін дії  10 секунд
+  date.setTime(date.getTime() + 10 * 1000); 
+  const cookieString = `userInfo=${encoded}; expires=${date.toUTCString()}; path=/`;
+  document.cookie = cookieString;
+
+  //Вивожу кукі в консоль 
+  console.log(`Cookie: ${cookieString}`) 
+  document.write(`<br> Cookie: ${cookieString}   <br>`) 
 }
-// Демонстрація використання
-const uniqueUsernames = new UniqueUsernames()
-uniqueUsernames.addUser('john_doe')
-uniqueUsernames.addUser('jane_doe')
-uniqueUsernames.addUser('john_doe') // Ця дія не змінить набір, оскільки 'john_doe' вже існує
-document.write(`<br> Додати john_doe: ${uniqueUsernames.addUser('john_doe')}<br>`)
-console.log(`Існує 'john_doe': ${uniqueUsernames.exists('john_doe')}`) // true
-document.write(`<br> Чи існує john_doe : ${uniqueUsernames.exists('john_doe')}<br>`)
-console.log(`Кількість унікальних імен: ${uniqueUsernames.count()}`) // 2
-document.write(`<br> Кількість унікальних імен: ${uniqueUsernames.count()}<br>`)
-// Експорт для використання в тестах
-// export { CalorieCalculator, UniqueUsernames }
+
+// Демонстрація використання функції
+setUserInfoCookie('language', 'en');
+
+
+
+/*
+ * #3
+ *
+ * Задача: Робота з sessionStorage для зберігання та отримання даних користувача
+ * Мета: Створити дві функції, saveUserInfo і getUserInfo, для взаємодії з sessionStorage. Перша функція повинна зберігати інформацію про користувача, а друга - отримувати її. Крім того, обидві функції повинні виводити відповідні повідомлення у консоль про успішне збереження або отримання даних.
+ *
+ * Вимоги до saveUserInfo:
+ *
+ * 1. Функція приймає два параметри: ключ (key) та значення (value).
+ * 2. Зберігає пару ключ-значення в sessionStorage.
+ * 3. Виводить у консоль повідомлення формату "Saved key: value".
+ *
+ * Вимоги до getUserInfo:
+ *
+ * 1. Функція приймає один параметр: ключ (key).
+ * 2. Отримує значення за вказаним ключем з sessionStorage.
+ * 3. Виводить у консоль повідомлення формату "Retrieved key: value", де value - це значення, отримане з sessionStorage.
+ * 4. Повертає значення отримане з sessionStorage.
+ */
+
+function saveUserInfo(key, value) {
+
+  // зберігання в sessionStorage 
+  sessionStorage.setItem(key, value) 
+}
+
+function getUserInfo(key) {
+
+  // Провірка елементу по ключу, якщо не зщнайден  виведе `Ключ не знайдено` якщо знайден виведе його
+  if(sessionStorage.getItem(key) === null) return `Ключ не знайдено`; 
+  return `Retrieved key (${key}): ${sessionStorage.getItem(key)}`
+}
+
+saveUserInfo('username', 'JohnDoe');
+console.log(getUserInfo('username'))
+document.write(`<br>` + getUserInfo('username') + `<br>`) 
+console.log(getUserInfo('usernamee'))
+document.write(`<br>` + getUserInfo('usernamee') + `<br>`)
+
+// export { createDomElement, setUserInfoCookie, saveUserInfo, getUserInfo }
